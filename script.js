d178057029dd08bf0e3522d4e66cc6076782fc9b
daviskb <davis.kingwell.brown@gmail.com>
2022-06-27 10:14:35 -0700

    got expanding menu working properly in css. finished basic html.  adding more JS now

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("test")
+
+function projects() {
+    var pj = document.getElementById("completed");
+    if (pj.style.display === "none") {
+      pj.style.display = "block";
+    } else {
+      pj.style.display = "none";
+    }
+  }
+
+  function wip() {
+    var wp = document.getElementById("wip");
+    if (wp.style.display === "none") {
+      wp.style.display = "block";
+    } else {
+      wp.style.display = "none";
+    }
+  }
+  function socials() {
+    var sc = document.getElementById("socials");
+    if (sc.style.display === "none") {
+      sc.style.display = "block";
+    } else {
+      sc.style.display = "none";
+    }
+  }
+  
